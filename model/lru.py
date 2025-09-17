25c8494240dfda377992418d0fc503c7a4b31375
ngoctuannguyen1980123@gmail.com <ngoctuannguyen1980123@gmail.com>
2025-09-17 23:13:47 +0700

    feat: No PFFN

--- model/lru.py
+++ model/lru.py
@@ -128,7 +128,7 @@ class LRUBlock(nn.Module):
     
     def forward(self, x, mask):
         x = self.lru_layer(x, mask)
-        x = self.feed_forward(x)
+        # x = self.feed_forward(x)
         return x
     
 
