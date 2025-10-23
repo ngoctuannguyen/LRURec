18bc4082844e7380ba337fdf434d902d26f69f22
ngoctuannguyen1980123@gmail.com <ngoctuannguyen1980123@gmail.com>
2025-10-23 13:50:56 +0700

    feat: add KAN layer

--- model/lru.py
+++ model/lru.py
@@ -204,12 +204,37 @@ class SwiGLU(nn.Module):
         output = self.linear2(gate * activated)
         return output
 
+class NaiveFourierKANLayer(nn.Module):
+    def __init__(self, inputdim, outdim, gridsize=16):
+        super(NaiveFourierKANLayer, self).__init__()
+        self.gridsize = gridsize
+        self.inputdim = inputdim
+        self.outdim = outdim
+
+        self.fouriercoeffs = nn.Parameter(torch.randn(2, outdim, inputdim, gridsize) /
+                                          (np.sqrt(inputdim) * np.sqrt(self.gridsize)))
+    def forward(self, x):
+        xshp = x.shape
+        outshape = xshp[0:-1] + (self.outdim,)
+        x = x.view(-1, self.inputdim)
+        # Starting at 1 because constant terms are in the bias
+        k = torch.reshape(torch.arange(1, self.gridsize + 1, device=x.device), (1, 1, 1, self.gridsize))
+        xrshp = x.view(x.shape[0], 1, x.shape[1], 1)
+        # This should be fused to avoid materializing memory
+        c = torch.cos(k * xrshp)
+        s = torch.sin(k * xrshp)
+        c = torch.reshape(c, (1, x.shape[0], x.shape[1], self.gridsize))
+        s = torch.reshape(s, (1, x.shape[0], x.shape[1], self.gridsize))
+        y = torch.einsum("dbik,djik->bj", torch.concat([c, s], axis=0), self.fouriercoeffs)
+
+        y = y.view(outshape)
+        return y
 
 class PositionwiseFeedForward(nn.Module):
     def __init__(self, d_model, d_ff, dropout=0.1):
         super().__init__()
-        self.w_1 = nn.Linear(d_model, d_ff * 2)
-        self.w_2 = nn.Linear(d_ff, d_model)
+        self.w_1 = NaiveFourierKANLayer(d_model, d_ff * 2)
+        self.w_2 = NaiveFourierKANLayer(d_ff, d_model)
         self.dropout = nn.Dropout(dropout)
         self.layer_norm = nn.LayerNorm(d_model)
 
